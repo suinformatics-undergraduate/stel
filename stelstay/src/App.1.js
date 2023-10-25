79360cc37df611f965982fbc4f2529f1a21bd243
WendyNG2001 <141152756+WendyNG2001@users.noreply.github.com>
2023-10-25 11:23:12 +0000

    Good morning

--- stelstay/src/App.1.js
+++ stelstay/src/App.1.js
@@ -1,3 +1,4 @@
+/*
 import { useEffect } from 'react';
 import {client} from "./Library/pocketbase";
 
@@ -6,6 +7,32 @@ export function App() {
     .collection("users").getFullList()
     .then(res), console.log(res));
 },[]);
+
 return
 <div>HELLO</div>;
 
+*/
+
+import React, { useEffect, useState } from 'react';
+import { client } from "./Library/pocketbase";
+
+export default function App() {
+  useEffect(() => {
+    // Use the useEffect callback function to fetch data and handle the response
+    client.collection("users").getFullList()
+      .then(res => {
+        console.log(res); // Log the response data
+        // You can set the data in state if you want to render it in your component
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); // The empty dependency array means this effect runs only once when the component mounts.
+
+  return (
+    <div>
+      <p>HELLO</p>
+      {/* You can render data fetched from Pocketbase here */}
+    </div>
+  );
+}

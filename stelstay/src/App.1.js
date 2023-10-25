49a945b13af4e191afc2c5d22cab671fd5472b4f
WendyNG2001 <141152756+WendyNG2001@users.noreply.github.com>
2023-10-25 05:53:17 +0000

    sleep

--- stelstay/src/App.1.js
+++ stelstay/src/App.1.js
@@ -1,7 +1,11 @@
 import { useEffect } from 'react';
+import {client} from "./Library/pocketbase";
 
 export function App() {
   useEffect(() => client
     .collection("users").getFullList()
     .then(res), console.log(res));
-}
+},[]);
+return
+<div>HELLO</div>;
+
--- stelstay/src/App.js
+++ stelstay/src/App.js
@@ -9,5 +9,4 @@ function MyApp() {
     </div>
   );
 }
-
 export default MyApp;
